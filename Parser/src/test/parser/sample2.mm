4379dc107db5183757471b67821436adc20f0406
New-bottle <775712558@qq.com>
2018-04-07 15:11:36 +0800

    three sample.mm

---
+++ Parser/src/test/parser/sample2.mm
@@ -0,0 +1,16 @@
+int Wallace = 1 << 10;
+class sometimes {
+	int naive;
+	void make_money() {
+		this.naive++;
+	}
+}
+
+int main() {
+	sometimes keep = new sometimes;
+	keep.naive = 0;
+	while (getInt() < Wallace) {
+		kepp.make_money();
+	}
+	return 0;
+}
